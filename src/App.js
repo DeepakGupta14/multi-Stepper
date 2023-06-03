87ef45c5f7b0ea67d01debe5823372ea7cc41ba8
DeepakGupta14 <135435876+DeepakGupta14@users.noreply.github.com>
2023-06-03 22:46:58 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import MultiForm from "./components/MultiForm";
+
+function App() {
+  return (
+    <div className="App">
+      <MultiForm />
+    </div>
+  );
+}
+
+export default App;
